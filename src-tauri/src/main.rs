a6c156619210fec99bff7b9ede0934f4ba0d91c5
michaelbguo <michaelbguo@tencent.com>
2022-07-19 10:14:00 +0800

    feat: tauri app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,10 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
 )]
 
-mod app_state;
-mod storage;
-mod worker;
-
 fn main() {
-    tauri::Builder::default()
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
